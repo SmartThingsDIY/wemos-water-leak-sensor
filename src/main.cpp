25602ab390ae4936ee5ff64d934445c31858fbd5
iLyas <isbkch@gmail.com>
2020-11-05 19:43:53 -0500

    send readings to an ESP01

--- src/main.cpp
+++ src/main.cpp
@@ -1,99 +1,144 @@
+/**
+  Reads data from a water leakage sensor, and sends it
+  through pins 2 and 3 to a connected ESP-01 WiFi module
+  @author MecaHumArduino
+  @version 1.0
+*/
+
 #include <Arduino.h>
-#include "secrets.h"
-#include <time.h>
-#include <ESP8266WiFi.h>
+#include <ArduinoJson.h>
 #include <SoftwareSerial.h>
-#include <WiFiClientSecure.h>
 
-const int read = A0;
-int value;
+#define DEBUG true
 
-uint8_t DST = 0;
-WiFiClientSecure net;
+// ESP TX => Uno Pin 2
+// ESP RX => Uno Pin 3
+SoftwareSerial wifiBoardSerial(2, 3);
 
-unsigned long lastMillis = 0;
-time_t now;
-time_t nowish = 1510592825;
+// Sensor pins
+#define sensorPower 7
+#define sensorPin A0
 
-void NTPConnect(void)
+// Value for storing water level
+int val = 0;
+
+// **************
+int readSensor();
+String sendDataToWiFiBoard(String command, const int timeout, boolean debug);
+String prepareDataForWiFi(int level);
+void setup();
+void loop();
+// **************
+
+/**
+ * Build and return a JSON document from the sensor data
+ * @param level
+ * @return
+ */
+String prepareDataForWiFi(int level)
 {
-  Serial.print("Setting time using SNTP");
+  StaticJsonDocument<200> doc;
 
-  configTime(TIME_ZONE * 3600, DST * 3600, "pool.ntp.org", "time.nist.gov");
-  now = time(nullptr);
+  doc["water_level"] = String(level);
 
-  while (now < nowish) {
-    delay(500);
-    Serial.print(".");
-    now = time(nullptr);
-  }
+  char jsonBuffer[512];
+  serializeJson(doc, jsonBuffer);
 
-  Serial.println(" done!");
-
-  struct tm timeinfo;
-  gmtime_r(&now, &timeinfo);
-
-  Serial.print("Current time: ");
-  Serial.print(asctime(&timeinfo));
+  return jsonBuffer;
 }
 
-void connectToWiFi(String initString)
+/**
+ * Send data through Serial to ESP8266 module
+ * @param command
+ * @param timeout
+ * @param debug
+ * @return
+ */
+String sendDataToWiFiBoard(String command, const int timeout, boolean debug)
 {
-  Serial.print(initString);
+  String response = "";
 
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, pass);
+  wifiBoardSerial.print(command); // send the read character to the esp8266
 
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(1000);
+  long int time = millis();
+
+  while((time+timeout) > millis()) {
+    while(wifiBoardSerial.available()) {
+      // The esp has data so display its output to the serial window
+      char c = wifiBoardSerial.read(); // read the next character.
+      response+=c;
+    }
   }
 
-  Serial.println(" ok!");
+  if (debug) {
+    Serial.print(response);
+  }
+
+  return response;
 }
 
-void setup()
-{
-	// Begin serial communication
+
+/**
+ * This is a function used to get the reading
+ * @param level
+ * @return
+ */
+int readSensor() {
+  // Turn the sensor ON
+	digitalWrite(sensorPower, HIGH);
+	delay(10);
+  // Perform the reading
+	val = analogRead(sensorPin);
+
+  // Turn the sensor OFF
+	digitalWrite(sensorPower, LOW);
+
+  return val;
+}
+
+void setup() {
+  Serial.begin(9600);
+  wifiBoardSerial.begin(9600);
+
+	// Set D7 as an OUTPUT
+	pinMode(sensorPower, OUTPUT);
+
+	// Set to LOW so no power flows through the sensor
+	digitalWrite(sensorPower, LOW);
+
 	Serial.begin(9600);
-  	Serial.println("starting setup");
-
-	connectToWiFi(String("Attempting to connect to SSID: ") + String(ssid));
-
-  	NTPConnect();
 }
 
-void loop()
-{
-	value = analogRead(read); // Read data from analog pin and store it to value variable
+void loop() {
 
-	if (value <= 480) {
-		Serial.println("Water level: 0mm - Empty!");
-	}
-	else if (value > 480 && value <= 530) {
-		Serial.println("Water level: 0mm to 5mm");
-	}
-	else if (value > 530 && value <= 615) {
-		Serial.println("Water level: 5mm to 10mm");
-	}
-	else if (value > 615 && value <= 660) {
-		Serial.println("Water level: 10mm to 15mm");
-	}
-	else if (value > 660 && value <= 680) {
-		Serial.println("Water level: 15mm to 20mm");
-	}
-	else if (value > 680 && value <= 690) {
-		Serial.println("Water level: 20mm to 25mm");
-	}
-	else if (value > 690 && value <= 700) {
-		Serial.println("Water level: 25mm to 30mm");
-	}
-	else if (value > 700 && value <= 705) {
-		Serial.println("Water level: 30mm to 35mm");
-	}
-	else if (value > 705) {
-		Serial.println("Water level: 35mm to 40mm");
-	}
+  if (DEBUG == true) {
+    Serial.print("buffer: ");
+    if (wifiBoardSerial.available()) {
+      String espBuf;
+      long int time = millis();
 
-	delay(1000);
+      while((time+1000) > millis()) {
+        while (wifiBoardSerial.available()) {
+          // The esp has data so display its output to the serial window
+          char c = wifiBoardSerial.read(); // read the next character.
+          espBuf += c;
+        }
+      }
+      Serial.print(espBuf);
+    }
+    Serial.println(" endbuffer");
+  }
+
+	// get the reading from the function below and print it
+	int level = readSensor();
+
+  if (DEBUG == true) {
+    Serial.print("Water level: ");
+    Serial.println(level);
+  }
+
+  String preparedData = prepareDataForWiFi(level);
+  sendDataToWiFiBoard(preparedData, 1000, DEBUG);
+
+	delay(5000);
 }
