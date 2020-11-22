d9c14a0234f1dfc6dc176f927ee7c647760cdfc3
iLyas <isbkch@gmail.com>
2020-11-22 10:42:31 -0500

    make interval reads the standard emthod

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 /**
-  Reads data from a water leakage sensor. When water is detected
-  an alarm is sent to an MQTT Topic
+  Wakes up every 30sec to read data from the water sensor, when water is
+  detected, it connects to WiFi, then sends an alarm to an MQTT topic
   @author MecaHumArduino
   @version 1.0
 */
@@ -9,27 +9,40 @@
 #include "secrets.h"
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include <Ticker.h>
 
 #define DEBUG true // switch to "false" for production
+Ticker ticker;
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-// Sensor pins
-#define sensorPower D7
-#define sensorPin A0
+#define sensorPower D7 // Power pin
+#define sensorPin A0 // Analog Sensor pins
+#define durationSleep 30 // seconds
+#define NB_TRYWIFI 20 // WiFi connection retries
 
 int sensorData = 0;
 
 // **************
+void tick();
+void loop();
+void setup();
+int readSensor();
 void connectToHass();
 void connectToWiFi();
-int readSensor();
-void setup();
-void loop();
+void publishAlarmToHass(int waterLevel);
 // **************
+
+void tick()
+{
+    // toggle state
+    int state = digitalRead(LED_BUILTIN);  // get the current state of GPIO1 pin
+    digitalWrite(LED_BUILTIN, !state);     // set pin to the opposite state
+}
 
 void connectToWiFi()
 {
+    int _try = 0;
     WiFi.mode(WIFI_STA);
     WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
@@ -40,6 +53,13 @@ void connectToWiFi()
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
         Serial.print(".");
+        _try++;
+        if ( _try >= NB_TRYWIFI ) {
+            if (DEBUG == true) {
+                Serial.println("Impossible to connect WiFi, going to deep sleep");
+            }
+            ESP.deepSleep(durationSleep * 1000000);
+        }
     }
     if (DEBUG == true) {
         Serial.println("Connected to Wi-Fi");
@@ -48,22 +68,30 @@ void connectToWiFi()
 
 void connectToHass()
 {
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
-    // If you do not want to use a username and password, change next line to
-    // if (client.connect("ESP8266Client")) {
-    if (client.connect("ESP8266Client", MQTT_USER, MQTT_PASSWORD)) {
-      Serial.println("connected");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
+    client.setServer(MQTT_SERVER, 1883);
+
+    // Loop until we're reconnected
+    while (!client.connected()) {
+        if (DEBUG == true) {
+            Serial.print("Attempting MQTT connection...");
+        }
+        // Attempt to connect
+        // If you do not want to use a username and password, change next line to
+        // if (client.connect("ESP8266Client")) {
+        if (client.connect("ESP8266Client", MQTT_USER, MQTT_PASSWORD)) {
+            if (DEBUG == true) {
+                Serial.println("connected");
+            }
+        } else {
+            if (DEBUG == true) {
+                Serial.print("failed, rc=");
+                Serial.print(client.state());
+                Serial.println(" try again in 5 seconds");
+            }
+            // Wait 5 seconds before retrying
+            delay(5000);
+        }
     }
-  }
 }
 
 /**
@@ -79,43 +107,56 @@ int readSensor()
     // Perform the reading
 	sensorData = analogRead(sensorPin);
 
-    // Turn the sensor OFF
+    // Set to LOW to turn the sensor OFF
 	digitalWrite(sensorPower, LOW);
 
   return sensorData;
 }
 
+void publishAlarmToHass(int waterLevel)
+{
+    // publish the reading to Hass through MQTT
+    client.publish(MQTT_PUBLISH_TOPIC, String(waterLevel).c_str(), true);
+    client.loop();
+    if (DEBUG == true) {
+        Serial.println("Alarm sent to Hass!");
+    }
+}
 void setup()
 {
-	Serial.begin(115200);
-    connectToWiFi();
-    client.setServer(MQTT_SERVER, 1883);
-
-    // Set D7 as an OUTPUT
-    pinMode(sensorPower, OUTPUT);
-
-    // Set to LOW so no power flows through the sensor
-    digitalWrite(sensorPower, LOW);
-}
-
-void loop()
-{
-    if (!client.connected()) {
-        connectToHass();
+    if (DEBUG == true) {
+        Serial.print("Waking up ");
     }
 
-    // get the reading from the function below and print it
-	int waterLevel = readSensor();
+    pinMode(sensorPower, OUTPUT); // Set D7 as an OUTPUT
+    digitalWrite(sensorPower, LOW);
 
+    ticker.attach(0.5, tick);
+
+	Serial.begin(115200);
+
+    // Wake up the leak sensor & get a reading
+    int waterLevel = readSensor();
     if (DEBUG == true) {
         Serial.print("Water Level: ");
         Serial.println(waterLevel);
     }
 
-    if (waterLevel > 2) {
-        client.loop();
-        client.publish(MQTT_PUBLISH_TOPIC, String(waterLevel).c_str(), true);
+    // If water is detected
+    if (waterLevel > 1) {
+        connectToWiFi();
+        connectToHass();
+        publishAlarmToHass(waterLevel);
     }
 
-    delay(1000);
+    if (DEBUG == true) {
+        Serial.println("Going to deep sleep now");
+    }
+
+    ESP.deepSleep(durationSleep * 1000000);
+}
+
+void loop()
+{
+
 }
